7d83a9b2e7529e3382a8fb285dd940245506e6ba
Xzavian Slaughter <77251397+xslau@users.noreply.github.com>
2022-10-23 20:11:26 -0400

    Add files via upload

---
+++ views/startScreen.py
@@ -0,0 +1,22 @@
+import pygame
+import viewInterface
+
+image = pygame.image.load("baseBG.jpeg")
+class startScreen():
+
+    def __init__(self, image):
+
+        self.image = image
+
+    def getImage():
+
+        return image
+'
+    def setImage(xImage):
+
+        image = xImage
+
+    def showScreen(): 
+
+        pygame.display.update()
+
---
+++ views/viewInterface.py
@@ -0,0 +1,20 @@
+import pygame
+
+class View():
+
+    def __init__(self, image):
+
+        self.image = image
+
+    def getImage():
+
+        return image
+'
+    def setImage(xImage):
+
+        image = xImage
+
+    def showScreen(): 
+
+        pygame.display.update()
+
