3c3feaad8b22acb8742e05ddfa6117fb15a60fee
bgrose <47512745+bgrose@users.noreply.github.com>
2022-11-01 18:55:28 -0400

    adding background and values

---
+++ assets/values.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+
+# Colors
+
+COLOR_Purple = (46, 41, 78)
+COLOR_Pink = (204, 167, 162)
+COLOR_Yellow = (233, 215, 88)
+COLOR_Orange = (175, 67, 25)
+COLOR_Red = (119, 32, 20)
+COLOR_White = (255, 255, 255)
+
+# Fonts
+FONT_Ethnocentric = os.path.join("assets", "fonts", "Ethnocentric.ttf")
+FONT_EthnocentricItalic = os.path.join("assets", "fonts", "EthnocentricItalic.ttf")
+
