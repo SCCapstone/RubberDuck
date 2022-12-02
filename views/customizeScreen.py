bb0376f29e825cc13ce02ef1365f2bc7151e5f22
bgrose <47512745+bgrose@users.noreply.github.com>
2022-12-01 19:07:05 -0500

    format

--- views/customizeScreen.py
+++ views/customizeScreen.py
@@ -46,12 +46,16 @@ def customize_screen(noises):
     #Coins available text - import actaul amount from JSON
     coins_text_image = subtitleFont.render("Coins:   500", True,
                                            values.COLOR_Yellow)
-    coinsCords = (left + screen.get_width()*.1215, screen.get_height() / 16 * 3)
+    coinsCords = (left + screen.get_width() * .1215,
+                  screen.get_height() / 16 * 3)
     screen.blit(coins_text_image, coinsCords)
     #coins asset next to available coins
     coins = pygame.image.load("assets/sprites/Coin.png")
-    coins = pygame.transform.scale(coins, (int(screen.get_width()*.035),int(screen.get_width()*.035)))
-    screen.blit(coins, (coinsCords[0] +screen.get_width() *.1273, coinsCords[1]))
+    coins = pygame.transform.scale(
+        coins,
+        (int(screen.get_width() * .035), int(screen.get_width() * .035)))
+    screen.blit(coins,
+                (coinsCords[0] + screen.get_width() * .1273, coinsCords[1]))
 
     #TODO add coin asset plus amount of coins available
 
@@ -62,14 +66,16 @@ def customize_screen(noises):
     swagDuck = pygame.image.load("assets/sprites/swagDuck.png")
 
     swagDuck = pygame.transform.scale(swagDuck, (purchaseSize, purchaseSize))
-    silhouetteDuck = pygame.transform.scale(silhouetteDuck, (purchaseSize, purchaseSize))
+    silhouetteDuck = pygame.transform.scale(silhouetteDuck,
+                                            (purchaseSize, purchaseSize))
     baseDuck = pygame.transform.scale(baseDuck, (purchaseSize, purchaseSize))
 
     #scale base skins for preview
-    previewSize = int(values.screenX*.28935)
+    previewSize = int(values.screenX * .28935)
     bigBaseDuck = pygame.transform.scale(baseDuck, (previewSize, previewSize))
-    bigSwagDuck =  pygame.transform.scale(swagDuck, (previewSize, previewSize))
-    bigSilhouetteDuck = pygame.transform.scale(silhouetteDuck, (previewSize, previewSize))
+    pygame.transform.scale(swagDuck, (previewSize, previewSize))
+    bigSilhouetteDuck = pygame.transform.scale(silhouetteDuck,
+                                               (previewSize, previewSize))
 
     #TODO Preview of Duck in Current State
     screen.blit(bigBaseDuck, (screen.get_width() / 3, screen.get_height() / 4))
@@ -77,12 +83,12 @@ def customize_screen(noises):
     #TODO Purchase / Equip Skin
 
     #TODO Left/Right arrows to browse skins to purchase / equip
-    xArr1 = math.floor(screen.get_width()*.3183)
-    yArr = math.floor(screen.get_height()*.6267)
-    wArr = math.floor(screen.get_width()*.0579)
-    hArr = math.floor(screen.get_height()*.0895)
-    sep = math.floor(screen.get_width()*.0289)
-    box = math.floor(screen.get_width()*.1447)
+    xArr1 = math.floor(screen.get_width() * .3183)
+    yArr = math.floor(screen.get_height() * .6267)
+    wArr = math.floor(screen.get_width() * .0579)
+    hArr = math.floor(screen.get_height() * .0895)
+    sep = math.floor(screen.get_width() * .0289)
+    box = math.floor(screen.get_width() * .1447)
 
     leftArr = pygame.image.load("assets/sprites/Back_Arrow.png")
     bigArr = pygame.transform.scale(leftArr, (wArr, hArr))
@@ -97,7 +103,9 @@ def customize_screen(noises):
         equip_text_image = smallFont.render("Equip", True, values.COLOR_Yellow)
     else:
         equip_text_image = smallFont.render("Equip", True, values.COLOR_Purple)
-    screen.blit(equip_text_image, (xArr1 + wArr + sep + screen.get_width()*.0434, yArr + screen.get_height()*.0313))
+    screen.blit(equip_text_image,
+                (xArr1 + wArr + sep + screen.get_width() * .0434,
+                 yArr + screen.get_height() * .0313))
 
     rightArr = pygame.transform.rotate(bigArr, 180)
     screen.blit(rightArr, (xArr1 + sep + sep + box + wArr, yArr))
@@ -106,21 +114,33 @@ def customize_screen(noises):
     #will make these box objects for an array so we can move with arrows, know which ones have been purchased.
     numboxes = 10
     for i in range(numboxes):
-        pygame.draw.rect(screen, values.COLOR_Pink,
-                         ((i + 1) * screen.get_width()*.1157 - screen.get_width()*.0289, screen.get_height()*.7610, screen.get_width()*.0868, screen.get_width()*.0868), 0)
+        pygame.draw.rect(
+            screen, values.COLOR_Pink,
+            ((i + 1) * screen.get_width() * .1157 - screen.get_width() * .0289,
+             screen.get_height() * .7610, screen.get_width() * .0868,
+             screen.get_width() * .0868), 0)
         if (i == 0):
-            screen.blit(baseDuck, ((i + 1) * screen.get_width()*.1157 - screen.get_width()*.0607, screen.get_height()*.7117))
+            screen.blit(
+                baseDuck,
+                ((i + 1) * screen.get_width() * .1157 -
+                 screen.get_width() * .0607, screen.get_height() * .7117))
         elif (i == 1):
-            screen.blit(swagDuck, ((i + 1) * screen.get_width()*.1157 - screen.get_width()*.0607, screen.get_height()*.7117))
+            screen.blit(
+                swagDuck,
+                ((i + 1) * screen.get_width() * .1157 -
+                 screen.get_width() * .0607, screen.get_height() * .7117))
         else:
-            screen.blit(silhouetteDuck, ((i + 1) * screen.get_width()*.1157 - screen.get_width()*.0607, screen.get_height()*.7117))
+            screen.blit(
+                silhouetteDuck,
+                ((i + 1) * screen.get_width() * .1157 -
+                 screen.get_width() * .0607, screen.get_height() * .7117))
 
     #4 boxes to change screen to purchase base skins, hats, trails, and backgrounds
-    xCord = math.floor(screen.get_width()*.0289)
-    yCord = math.floor(screen.get_height()*.1791)
-    width = math.floor(screen.get_width()*.1591)
-    height = math.floor(screen.get_height()*.0895)
-    separation = math.floor(screen.get_height()*.0448)
+    xCord = math.floor(screen.get_width() * .0289)
+    yCord = math.floor(screen.get_height() * .1791)
+    width = math.floor(screen.get_width() * .1591)
+    height = math.floor(screen.get_height() * .0895)
+    separation = math.floor(screen.get_height() * .0448)
 
     #Base Skins Box
     pygame.draw.rect(screen, values.COLOR_Pink, (xCord, yCord, width, height),
@@ -134,7 +154,8 @@ def customize_screen(noises):
     else:
         BS_text_image = smallFont.render("Base Skins", True,
                                          values.COLOR_Purple)
-    screen.blit(BS_text_image, (xCord + screen.get_width()*.0174, yCord + screen.get_height()*.0313))
+    screen.blit(BS_text_image, (xCord + screen.get_width() * .0174,
+                                yCord + screen.get_height() * .0313))
 
     #Hats Box
     pygame.draw.rect(screen, values.COLOR_Pink,
@@ -149,7 +170,8 @@ def customize_screen(noises):
     else:
         Hats_text_image = smallFont.render("Hats", True, values.COLOR_Purple)
     screen.blit(Hats_text_image,
-                (xCord + screen.get_width()*.0521 , yCord + screen.get_height()*.0313 + (height + separation) * 1))
+                (xCord + screen.get_width() * .0521, yCord +
+                 screen.get_height() * .0313 + (height + separation) * 1))
 
     #Trails Box
     pygame.draw.rect(screen, values.COLOR_Pink,
@@ -166,7 +188,8 @@ def customize_screen(noises):
         Trails_text_image = smallFont.render("Trails", True,
                                              values.COLOR_Purple)
     screen.blit(Trails_text_image,
-                (xCord + screen.get_width()*.0405, yCord + screen.get_height()*.0313 + (height + separation) * 2))
+                (xCord + screen.get_width() * .0405, yCord +
+                 screen.get_height() * .0313 + (height + separation) * 2))
 
     #Backgrounds Box
     pygame.draw.rect(screen, values.COLOR_Pink,
@@ -183,7 +206,8 @@ def customize_screen(noises):
         Backgrounds_text_image = smallFont.render("Backgrounds", True,
                                                   values.COLOR_Purple)
     screen.blit(Backgrounds_text_image,
-                (xCord + screen.get_width()*.0029, yCord + screen.get_height()*.0313 + (height + separation) * 3))
+                (xCord + screen.get_width() * .0029, yCord +
+                 screen.get_height() * .0313 + (height + separation) * 3))
 
     # Coordinates Home button
     homeCords = (values.screenX * .0065, values.screenY * .011)
@@ -195,16 +219,16 @@ def customize_screen(noises):
         SR_text_image = subtitleFont.render("HOME", True, values.COLOR_Pink)
     screen.blit(SR_text_image, (homeCords[0], homeCords[1]))
 
-
     # check for mouse click
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             menuS.double_click_preventer()
             if event.button == 1:
                 # check if mouse is in rect
-                if homeCords[0] < pygame.mouse.get_pos(
-                )[0] < homeCords[0] + (right - left - screen.get_width()*.0231) / 3 and homeCords[
-                        1] < pygame.mouse.get_pos()[1] < homeCords[1] + screen.get_height()*.0448:
+                if homeCords[0] < pygame.mouse.get_pos()[0] < homeCords[0] + (
+                        right - left - screen.get_width() *
+                        .0231) / 3 and homeCords[1] < pygame.mouse.get_pos(
+                        )[1] < homeCords[1] + screen.get_height() * .0448:
                     # return to home screen
                     noises.playSound("quack")
                     menuS.set_game_menu(menuS.menu.HOME)
