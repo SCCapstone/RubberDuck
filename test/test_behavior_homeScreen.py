ee0a4c2804a7e29258255dc955d02600965d369f
bgrose <47512745+bgrose@users.noreply.github.com>
2023-01-29 14:27:17 -0500

    movement

--- test/test_behavior_homeScreen.py
+++ test/test_behavior_homeScreen.py
@@ -19,7 +19,18 @@ def test_home_Screen():
 @when('the player clicks the Start Game button')
 def test_click_StartGame():
     pygame.init()
+    # Make game full screen
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Ducks In Space")
+    pygame.display.update()
     homeScreen.check_click(.253, .498, .761, .797, soundHandler.SFXHandler(), menuS.menu.GAME)
+    #print screenX, screenY
+    print(pygame.display.get_surface().get_size()[0], pygame.display.get_surface().get_size()[1])
+    # move mouse to start game button
+    pygame.mouse.set_pos(values.screenX*.300, values.screenY*.765)
+    pygame.mouse.set_visible(True)
+    print(pygame.mouse.get_pos())
+    pygame.mouse.set_pressed((1, 0, 0))
     
 @then('the game should start')
 def test_game_start():
