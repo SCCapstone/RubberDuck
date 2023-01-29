ec83c96960d6a325a47cb7f46d3647d66775b278
ddkeen <d3keen@gmail.com>
2023-01-28 23:33:00 -0500

    first behavior test: ModuleNotFound erros

--- test/behavior_tests/features/homeScreen.feature
+++ test/behavior_tests/features/homeScreen.feature
@@ -3,27 +3,27 @@ Feature: Home screen
 
 Scenario: Start game
     Given Game is loaded
-    When the player clicks the 'Start Game' button
+    When the player clicks the Start Game button
     Then a new game is started
 
 Scenario: Access Settings screen
     Given Game is loaded
-    When the player clicks the 'Settings' button
+    When the player clicks the Settings button
     Then the view switches to the settings screen
 
 Scenario: Access High Scores screen
     Given Game is loaded
-    When the player clicks the 'High Scores' button
+    When the player clicks the High Scores button
     Then the view switches to the high score screen
 
 Scenario: Access Customize screen
     Given Game is loaded
-    When the player clicks the 'Customize' button
+    When the player clicks the Customize button
     Then the view switches to the customize screen
 
 Scenario: Access Stats screen
     Given Game is loaded
-    When the player clicks the 'Stats' button
+    When the player clicks the Stats button
     Then the view switches to the stats screen
 
 Scenario: Quit game
--- test/behavior_tests/step_defs/test_homeScreen.py
+++ test/behavior_tests/step_defs/test_homeScreen.py
@@ -1,27 +1,24 @@
 #import pytest_bdd methods and feature file for this test
 import pytest
-from pytest_bdd import scenario, scenarios, given, when, then, parsers
-from pathlib import Path
-#import specific feature file from features folder in test
-featureFileDir='features'
-featureFile='homeScreen.feature'
-BASE_DIR = Path(__file__).resolve().parent
-FEATURE_FILE = BASE_DIR.joinpath(featureFileDir).joinpath(featureFile)
-    
-scenario(FEATURE_FILE, 'Start game')
+from pytest_bdd import *
 
-def test_start_game():
-    print("End of Start game test")
-    pass
+import menuStructure as menuS
+from views import homeScreen
+from assets import values
+
+
+scenarios('../features/homeScreen.feature')
 
 @given('Game is loaded')
-def current_view():
-    #get to home screen
+def home_Screen():
+    menuS.set_game_menu(menuS.menu.HOME)
     
-@when(the player clicks the 'Start Game' button)
-def button_click():
-    #click the 'Start Game' button
-
+@when('the player clicks the Start Game button')
+def click_StartGame():
+    homeScreen.check_click(.253, .498, .761, .797, noises, menuS.menu.GAME)
+    
 @then('a new game is started')
 def final_view():
-    assert 
+    assert menuS.get_game_menu == 3
+
+#we will be able to paremeterize this one test to work for all scenarios in the feature (swtich to all other screens)
