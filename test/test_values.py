9cdf097b80516046b5a3a79c0689e1d5850e2186
bgrose <47512745+bgrose@users.noreply.github.com>
2023-04-12 18:25:28 -0400

    closing Values Unit Test #226

--- test/test_values.py
+++ test/test_values.py
@@ -12,14 +12,8 @@ def test_set_screen_size():
     assert values.SCREEN_SIZE == (500, 600)
 
 
-"""def test_sessionRunTime():
-        global startTime
-        startTime = time.time()
-        with patch('time.gmtime') as mock_gmtime:
-            mock_gmtime.return_value = time.struct_time([0, 0, 0, 1, 0, 0, 0, 0, 0])
-            result = values.sessionRunTime()
-        assert result == "01:00"
-"""
+def test_sessionRunTime():
+    assert values.sessionRunTime() == "00:00"
 
 
 def test_setStartTime():
@@ -38,16 +32,29 @@ def test_resetGameScore():
     assert values.game_score == 0
 
 
-def test_setCoinsinGame():
-    values.setCoinsinGame(10)
+def test_getBG():
+    assert values.getBG(0) == "assets/backgrounds/base_bg.jpg"
+    assert values.getBG(1) == "assets/backgrounds/pixelNebula.png"
+    assert values.getBG(2) == "assets/backgrounds/pixelSpace.png"
+    
+def test_updateCoins():
+    values.coins_in_game = 10
+    values.updateCoins()
     assert values.coins_in_game == 10
 
-
-def test_resetCoinsinGame():
-    values.resetCoinsinGame()
-    assert values.coins_in_game == 0
-
+def test_setBackground():
+    values.setBackground("bg1")
+    assert values.current_background == "bg1"
 
 def test_setSkin():
     values.setSkin("skin1")
     assert values.current_skin == "skin1"
+
+def test_resetCoinsinGame():
+    values.resetCoinsinGame()
+    assert values.coins_in_game == 0
+    
+def test_setCoinsinGame():
+    values.setCoinsinGame(10)
+    assert values.coins_in_game == 10
+
