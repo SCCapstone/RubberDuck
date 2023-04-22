5b57c6c1f2c820a21ba65210715c5f6bc53749ed
bgrose <47512745+bgrose@users.noreply.github.com>
2023-04-22 12:37:28 -0400

    close Game Screen Behavioral Test #244

--- test/features/gameScreen.feature
+++ test/features/gameScreen.feature
@@ -1,41 +1,27 @@
 Feature: Game screen
     Tests whether player can perform all actions required during gameplay
 
-Scenario: Pause Game
-Given the game is running
-When the player presses the pause button
-Then the game should pause and the pause menu should appear
+Scenario: Start Game
+Given the game is called to splash screen
+When the player presses the start button
+Then the game should start and the game screen should appear
 
 Scenario: Fire rocket
 Given the game is running
-When the player left clicks
+When the player presses the fire button
 Then a rocket should be launched from the player's position towards where the mouse is pointing
 
-Scenario: Swept off screen
+Scenario: Lose a heart
 Given the game is running
-When the player moves to the edge of the screen
-Then the player should die, the game should end, and the game over menu should appear.
-Scenario: Collect Coin
-Given the game is running and a coin is present
-When the player moves over the coin
-Then the coin should disappear and the player's score and number of coins should increase
-
-Scenario: Collect health
-Given the game is running, a heart is present, and player health is not full
-When the player moves over the heart
-Then the heart should disappear and the player's health should increase
-
-Scenario: Collect SpeedUp
-Given the game is running and a speed boost is present
-When the player moves over the speed boost
-Then the speed boost should disappear and the player's movement speed should increase temporarily
-
-Scenario: Hit by enemy
-Given the game is running and an enemy is present
-When the player collides with the enemy
-Then the player's health should decrease and the enemy should disappear
-
-Scenario: Lose all hearts
-Given the game is running and the player has 1 health left
 When the player collides with an enemy
-Then the player should die, the game should end, and the game over menu should appear.
+Then the player should lose a heart
+
+Scenario: Pause Game
+Given the game is running
+When the player presses the pause button
+Then the game should pause and the pause screen should appear
+
+Scenario: Lose the game
+Given the game is running
+When the player loses all hearts
+Then the game should end and the game over screen should appear
--- test/test_behavior_gameScreen.py
+++ test/test_behavior_gameScreen.py
@@ -1,7 +1,171 @@
 import sys
+import time
 
 sys.path.append('..')
-#import pytest_bdd methods and feature file for this test
+from pytest_bdd import *
+import pygame
 
+import menuStructure as menuS
+from assets import soundHandler
+from views import gameScreen
+global value_passer, game
 #scenario('../test/features/gameScreen.feature', '<scenario name>')
 #Use given when then structure for tests
+
+@scenario('../test/features/gameScreen.feature', 'Start Game')
+
+@given('the game is called to splash screen')
+def test_game_is_running():
+    pygame.init()
+    # Make game full screen
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Ducks In Space")
+    pygame.display.update()
+    menuS.set_game_menu(menuS.menu.GAME)
+    
+@when('the player presses the start button') 
+def test_click_start():
+    global value_passer
+    game = gameScreen.Game() #call gameScreen function
+    game.reset()
+    # Simulate an escape key click
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+    
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+
+    value_passer = game
+    
+@then('the game should start and the game screen should appear')
+def test_game_is_running():
+    global value_passer
+    assert value_passer.stage == value_passer.PLAYING
+    value_passer = None
+    
+    
+@scenario('../test/features/gameScreen.feature', 'Fire rocket')
+
+@given('the game is running')
+def test_game_is_running():
+    global game
+    pygame.init()
+    # Make game full screen
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Ducks In Space")
+    pygame.display.update()
+    menuS.set_game_menu(menuS.menu.GAME)
+    gameScreen.noiseMaker = soundHandler.SFXHandler()
+    game = gameScreen.Game() #call gameScreen function
+    game.reset()
+    # Simulate an escape key click
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+    
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+
+@when('the player presses the fire button')
+def test_click_rocket():
+    global game
+    game.duck.shoot()
+
+@then('a rocket should be launched from the player\'s position towards where the mouse is pointing')
+def test_rocket_is_fired():
+    global game
+    assert game.duck.rockets is not None
+
+@scenario('../test/features/gameScreen.feature', 'Lose a heart')
+@given('the game is running')
+def test_game_is_running():
+    global game
+    pygame.init()
+    # Make game full screen
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Ducks In Space")
+    pygame.display.update()
+    menuS.set_game_menu(menuS.menu.GAME)
+    gameScreen.noiseMaker = soundHandler.SFXHandler()
+    game = gameScreen.Game() #call gameScreen function
+    game.reset()
+    # Simulate an escape key click
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+    
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+
+@when('the player collides with an enemy')
+def test_collide():
+    global game
+    game.duck.takeDamage(1)
+    
+@then('the player should lose a heart')
+def test_heart_is_lost():
+    global game
+    assert game.duck.health == 2
+    
+@scenario('../test/features/gameScreen.feature', 'Pause Game')
+@given('the game is running')
+def test_game_is_running():
+    global game
+    pygame.init()
+    # Make game full screen
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Ducks In Space")
+    pygame.display.update()
+    menuS.set_game_menu(menuS.menu.GAME)
+    gameScreen.noiseMaker = soundHandler.SFXHandler()
+    game = gameScreen.Game()
+    game.reset()
+    # Simulate an escape key click
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+
+@when('the player presses the pause button')
+def test_click_pause():
+    global game
+    # Simulate an escape key click
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+    
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+    
+@then('the game should pause and the pause screen should appear')
+def test_game_is_paused():
+    global game
+    assert game.stage == game.PAUSED
+    game.reset()
+
+
+@scenario('../test/features/gameScreen.feature', 'Lose the game')   
+@given('the game is running')
+def test_game_is_running():
+    global game
+    pygame.init()
+    # Make game full screen
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Ducks In Space")
+    pygame.display.update()
+    menuS.set_game_menu(menuS.menu.GAME)
+    gameScreen.noiseMaker = soundHandler.SFXHandler()
+    game = gameScreen.Game()
+    game.reset()
+    # Simulate an escape key click
+    pygame.event.post(pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE))
+    game.process_events()
+
+
+@when('the player loses all hearts')
+def test_lose_all_hearts():
+    global game
+    game.duck.takeDamage(3)
+    game.process_events()
+    game.update()
+
+@then('the game should end and the game over screen should appear')
+def test_game_is_over():
+    global game
+    assert game.stage == game.GAME_OVER
+
